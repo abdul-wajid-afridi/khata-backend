c959625f7b3de0acbc5f64ade6f37dacdff626fc
abdul-wajid-afridi <wajidafridi8554@gmail.com>
2023-06-05 20:49:34 +0500

    cors-url-changed to live

--- Index.js
+++ Index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 7000;
 // all Middlewares
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://sarhad-tradders.netlify.app/",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
